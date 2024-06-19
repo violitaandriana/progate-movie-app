023b970791962b669da2e968e4ef36ab424ae52f
violitaandriana <violitaandriana2003@gmail.com>
2024-06-19 21:53:20 +0700

    feat: home screen with 4 categories

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,22 +1,54 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { ScrollView, View, StatusBar, StyleSheet } from 'react-native';
+import type { MovieListProps } from '../types/app';
+import MovieList from '../components/movies/MovieList';
+
+const movieLists: MovieListProps[] = [
+  {
+    title: 'Now Playing in Theater',
+    path: 'movie/now_playing?language=en-US&page=1',
+    coverType: 'backdrop',
+  },
+  {
+    title: 'Upcoming Movies',
+    path: 'movie/upcoming?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Top Rated Movies',
+    path: 'movie/top_rated?language=en-US&page=1',
+    coverType: 'poster',
+  },
+  {
+    title: 'Popular Movies',
+    path: 'movie/popular?language=en-US&page=1',
+    coverType: 'poster',
+  },
+]
 
 export default function Home({ navigation }: any): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Movie Page</Text>
-      <Button
-        title="PERGI KE MOVIE DETAIL"
-        onPress={() => navigation.navigate("MovieDetail")}
-      />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {movieLists.map((movieList) => (
+          <MovieList 
+            title={movieList.title}
+            path={movieList.path}
+            coverType={movieList.coverType}
+            key={movieList.title}
+          />
+        ))}
+        <StatusBar translucent={false} />
+      </View>
+    </ScrollView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: StatusBar.currentHeight ?? 32,
     alignItems: 'center',
     justifyContent: 'center',
+    rowGap: 16,
   },
 });
